4e947fac040a330742c67181550ccf3a695a4d09
Yahia Mohamed <yahiamohamed21@users.noreply.github.com>
2026-07-27 06:02:15 +0300

    second updatet

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.API_BASE_URL || "http://localhost:5168"}/api/v1/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
